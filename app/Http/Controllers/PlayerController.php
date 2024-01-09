331f7bbc1b74fd9a39d311d16f61ce756b1b4ec1
tszyu93720 <tszyuloveyou@live.hk>
2024-01-09 14:58:19 +0800

    missing update preset contorller

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 
-class GameController extends BaseController {
+class PlayerController extends BaseController {
     public function index() {
         // ...
     }
