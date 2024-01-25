2b4f249430f18f96e647e62b7d10cf1741bd23b6
tszyu93720 <tszyuloveyou@live.hk>
2024-01-25 15:13:40 +0800

    update some logic for game

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,16 +3,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use App\Models\Contest;
+use App\Models\Game;
 
 class GameController extends BaseController {
+    protected $contest;
+    protected $maxGameNumber;
+    private function isLastGame( $request ) {
+        $this->maxGameNumber = Game::where( "contest_id", $request[ "contest" ] )
+            ->max( "game_number" );
+        $this->contest = Contest::with([
+                "games", "players" => function( $query ) {
+                    if( $this->maxGameNumber % 2 == 0 ) {
+                        $query->order( "id", "desc" );
+                    } else {
+                        $query->order( "id" );
+                    }
+                }
+             ])->findOrFail( $request[ "contest" ] );
+        if( $this->maxGameNumber == $this->contest[ "number_of_game" ] - 1 ) {
+            return true;
+        }
+        return false;
+    }
+    public function __construct() {
+        parent::__construct();
+        $this->middleware(
+            function( $request, $next ) {
+                if( $this->isLastGame( $request ) ) {
+                    return $next($request);
+                }
+                abort( 404 );
+            }
+        )->only( [ "create" ] );
+        $this->middleware(
+            function( $request, $next ) {
+                if( $this->isLastGame( $request ) ) {
+                    return $next($request);
+                }
+                return response()->json([
+                    "type" => "msg",
+                    "message" => "Just can create last game",
+                ]);
+            }
+        )->only( [ "update" ] );
+    }
     public function index() {
         // ...
     }
     public function create() {
-        // ...
+        //...
     }
-    public function store() {
-        // ...
+    public function store( Request $request ) {
+        if( $this->isLastGame( $request ) ) {
+            // ...
+        } else {
+            // ...
+        }
     }
     public function update() {
         // ...
